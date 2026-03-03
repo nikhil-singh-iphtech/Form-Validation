4ed91a511e862557e16193d265f58fdbd6685321
nikhil-singh-iphtech <nikhil.singh@iphtechnologies.com>
2026-03-03 11:04:46 +0530

    abc

--- script.js
+++ script.js
@@ -4,15 +4,35 @@ document.getElementById("userForm").addEventListener("submit", function(e) {
   document.querySelectorAll(".error").forEach(el => el.textContent = "");
 
   let firstname = document.getElementById("firstname").value.trim();
-  let middlename = document.getElementById("middlename").value.trim();
+  // let middlename = document.getElementById("middlename").value.trim();
   let lastname = document.getElementById("lastname").value.trim();
   let email = document.getElementById("email").value.trim();
   let contact = document.getElementById("contact").value.trim();
-  let age = document.getElementById("age").value.trim();
+  // let age = document.getElementById("age").value.trim();
   let description = document.getElementById("description").value.trim();
   let gender = document.querySelector('input[name="gender"]:checked');
 
+   const studentClass = document.getElementById("class").value;
+  const dob = document.getElementById("dob").value;
+
+
+  const classError = document.getElementById("classError");
+  const dobError = document.getElementById("dobError");
+  classError.textContent = "";
+  dobError.textContent = "";
+
   let isValid = true;
+
+   if (studentClass === "") {
+    classError.textContent = "Please select your class.";
+    isValid = false;
+  }
+
+ 
+  if (dob === "") {
+    dobError.textContent = "Please select your Date of Birth.";
+    isValid = false;
+  }
 
  
   if (firstname === "") {
@@ -20,35 +40,39 @@ document.getElementById("userForm").addEventListener("submit", function(e) {
     isValid = false;
   }
 
-  if (middlename === "") {
-    document.getElementById("middlenameError").textContent = "Middle name is required";
-    isValid = false;
-  }
+  
 
   if (lastname === "") {
     document.getElementById("lastnameError").textContent = "Last name is required";
     isValid = false;
   }
 
-
-  let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailPattern.test(email)) {
-    document.getElementById("emailError").textContent = "Enter valid email";
+   let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   if(email===""){
+    document.getElementById("emailError").textContent="Email field cannot be empty"
+    isValid=false;
+   }else if(!emailPattern.test(email)){
+     document.getElementById("emailError").textContent = "Enter valid email";
     isValid = false;
-  }
+   }
 
 
+
+  
+ 
   let contactPattern = /^[0-9]{10}$/;
-  if (!contactPattern.test(contact)) {
+  if(contact===""){
+    document.getElementById("contactError").textContent="Phone Number cannot be empty"
+    isValid=false
+  }else if(!contactPattern.test(contact)){
     document.getElementById("contactError").textContent = "Enter valid 10-digit number";
     isValid = false;
   }
 
+ 
 
-  if (age === "" || age < 1 ) {
-    document.getElementById("ageError").textContent = "Enter valid age";
-    isValid = false;
-  }
+
+ 
 
  
   if (!gender) {
@@ -65,11 +89,11 @@ document.getElementById("userForm").addEventListener("submit", function(e) {
   if (isValid) {
     const userData = {
       firstname,
-      middlename,
       lastname,
+      studentClass,
       email,
       contact,
-      age,
+      dob,
       gender: gender.value,
       description
     };
